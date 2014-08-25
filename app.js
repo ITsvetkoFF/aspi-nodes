f4cba3ee64f1b27ee6e9b052e00fba9e92df1d54
root <ihor@tsvetserver.itsve.tk>
2014-08-26 00:10:19 +0300

    fhg

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var app = angular.module("AspiNodesApp", []);
 app.controller('SimulationController', function ($scope) {
 
-  $scope.nodeQuantity = '5';
-  $scope.fieldWidth = '1000';
-  $scope.fieldHeight = '1000';
+    $scope.nodeQuantity = '5';
+    $scope.fieldWidth = '1000';
+    $scope.fieldHeight = '1000';
     $scope.fieldMinRange = '50';
     $scope.fieldMaxRange = '200';
 
@@ -12,10 +12,15 @@ app.controller('SimulationController', function ($scope) {
         showResults();
 
         function clearAndPopulate() {
-            $scope.nodes = [];
-            $scope.nodes = _.map(_.range($scope.nodeQuantity), function(num) {
-                return {x:Math.random()*$scope.fieldWidth, y:Math.random()*$scope.fieldHeight};
+            var data = d3.range(50000).map(function () {
+                return [Math.random() * $scope.fieldWidth, Math.random() * $scope.fieldHeight];
             });
+
+            var quadtree = d3.geom.quadtree()
+                .extent([[-1, -1], [$scope.fieldWidth, $scope.fieldHeight]])
+                (data);
+
+            $scope.nodes = quadtree;
         };
 
         function showResults() {
--- services.js
+++ services.js
@@ -1,4 +1,12 @@
-var underscore = angular.module('underscore', []);
-underscore.factory('_', function() {
-  return window._; // assumes underscore has already been loaded on the page
-});
+angular.module('underscore', [])
+    .factory('_', function () {
+        return window._; // assumes underscore has already been loaded on the page
+    });
+
+angular.module('d3', [])
+    .factory('d3Service', [
+            function () {
+                var d3;
+                // insert d3 code here
+                return d3;
+  }];
